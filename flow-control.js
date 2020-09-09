04c70edcc3c84db7c3e354abb3ccc8164af921bd
hugo matos <hugocfmatos@gmail.com>
2020-09-09 16:32:57 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,12 +1,26 @@
 function basicTeenager(age) {
-
+  if (age<=19 && age>=13) {
+console.log("You are a teenager!")
+  }
 }
 
 function teenager(age) {
-
+  if (age<=19 && age>=13) {
+console.log("You are a teenager!")
+  } else {
+    console.log("You are not a teenager")
+  }
 }
 
 function ageChecker(age) {
+  if (age<=19 && age>=13) {
+console.log("You are a teenager!")
+  } else if (age>19) {
+  console.log("You are an adult")
+}
+  else {
+    console.log("You are a kid")
+  }
 
 }
 
